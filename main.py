f7230780c39daaa7f703c9d2a25bffc6e621de71
guddn20 <128574570+guddn20@users.noreply.github.com>
2026-06-18 17:23:47 +0900

    first commit
    사람인/고용24 크롤링 진행 전 화면 만들기

---
+++ main.py
@@ -0,0 +1,100 @@
+import streamlit as st #스트림릿 페이지
+
+# 레이아웃(웹페이지의 생김새)
+# 스트림릿 웹페이지의 '헤더'역할
+st.set_page_config(page_title = "채용공고 자동 크롤링 서비스",
+                   layout = 'wide')
+
+st.title("채용 공고 조회 서비스")
+st.write("""이 서비스는 채용 공고를 조회하기 위해 만든 서비스 입니다.
+            \n사람인과 고용24 페이지에 검색어를 입력하여 자동으로 공고를 크롤링합니다.
+            \n크롤링한 내용은 하단의 표에서 확인 가능하며 csv파일로 다운로드할 수 있습니다."""
+)
+
+# 공고를 조회할 사이트를 선택
+site_select = st.radio("크롤링할 사이트 선택", ['사람인','고용24'],horizontal=True)
+
+# with st.sidebar:
+#   st.write('사이드 확장')
+
+with st.expander("상세 검색 조건", expanded=True):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        search_text = st.text_input("검색어를 입력", placeholder='예 : 파이썬, 인공지능')
+
+        except_text = st.text_input("제외할 검색어", placeholder='예 : 야간 근무, 출장')
+
+        max_pages = st.number_input('크롤링할 페이지 수', min_value=1, max_value=30)
+
+    with col2:
+        if site_select == '사람인':
+            # 사람인 사이트에서 '지역'을 의미하는 코드만 분리
+            loc_options = {
+                "전체": None,
+                "서울": "101000",
+                "경기": "102000",
+                "인천": "108000",
+                "부산": "106000",
+                "대구": "104000",
+                "광주": "103000",
+                "대전": "105000"
+            }
+
+            selected_location = st.multiselect('지역을 선택하세요.', list(loc_options.keys()), default=['전체'])
+
+            # st.write(selected_location)
+            # 크롤링 진행할 때 사용하기 위해 문자열 추출
+            # selected_location으로 '서울', '경기' 같은 '지역명' 추출
+            # loc_oprionts(딕셔너리)이므로, loc_options['부산'] => 값
+            # x는 '지역명'
+            # if 조건이 붙는 이유 : '지역명' 없는 '지역'이라면 추가가 안됨.
+            locations = [loc_options[x] for x in selected_location if loc_options[x]]
+
+            # st.write(locations)
+
+            # 직무 검색
+            cat_options = {
+                "전체": None,
+                "IT개발·데이터": "2",
+                "경영·사무": "3",
+                "마케팅·홍보": "4",
+                "디자인": "9",
+                "영업": "5"
+            }
+
+            selected_category = st.multiselect( "직무를 선택하세요", list(cat_options.keys()), default=["IT개발·데이터"])
+            category = [cat_options[x] for x in selected_category if cat_options[x]]
+
+            # 경력
+            career_options = {'전체':'0', '신입':'1', '경력':'2', '신입/경력':'3'}
+            selected_career = st.selectbox('경력을 선택하세요', list(career_options.keys()))
+            career = career_options[selected_career]
+
+            # 학력
+            edu_option = {"전체": "0", "고졸": "1", "대졸(2,3년)": "2", "대졸(4년)": "3", '석사':'4', '박사':'5'}
+            selected_edu = st.selectbox('학력을 선택하세요', list(edu_option.keys()))
+            edu = edu_option[selected_edu]
+
+        else : # 고용24
+            region = st.text_input('지역 코드를 입력하세요', value='11000')
+            occupation = st.text_input('직종 코드를 입력하세요', value='024', help='직종 코드 제한')
+
+            # 경력
+            career_options = {"전체": "A", "신입": "N", "경력": "E", "관계없음": "Z"}
+            career = st.selectbox('경력을 선택하세요', list(career_options.keys()))
+            career = career_options[career]
+            
+            # 학력
+            edu_options = {
+                "전체": "noEdu",
+                "중졸이하": "01,02",
+                "고졸": "03",
+                "대졸(2~3년)": "04",
+                "대졸(4년)": "05",
+                "석사": "06",
+                "박사": "07",
+                "학력무관": "00",
+            }
+            edu = st.selectbox('학력을 선택하세요', list(edu_options.keys()))
+            edu = edu_options[edu]
